de80f40e848c9b5b28e4e81bea5b4913cc4492d6
KenGoossens[MSFT] <44007552+KenGoossens@users.noreply.github.com>
2024-03-08 22:42:36 +0100

    Add files via upload

---
+++ AP_DetectionScripts.ps1
@@ -0,0 +1,48 @@
+﻿$TranscriptPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+$TranscriptName = "AutoPatchDetection.log"
+new-item $TranscriptPath -ItemType Directory -Force
+
+# stopping orphaned transcripts
+try
+{
+    stop-transcript|out-null
+}
+  catch [System.InvalidOperationException]
+{}
+
+Start-Transcript -Path $TranscriptPath\$TranscriptName -Append
+
+
+# initialize the array
+[PsObject[]]$regkeys = @()
+# populate the array with each object
+$regkeys += [PsObject]@{ Name = "DoNotConnectToWindowsUpdateInternetLocations"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"}
+$regkeys += [PsObject]@{ Name = "DisableWindowsUpdateAccess"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"}
+$regkeys += [PsObject]@{ Name = "WUServer"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"}
+$regkeys += [PsObject]@{ Name = "UseWUServer"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\"}
+$regkeys += [PsObject]@{ Name = "NoAutoUpdate"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\"}
+
+
+foreach ($setting in $regkeys)
+{
+    write-host "checking $($setting.name)"
+    if((Get-Item $setting.path -ErrorAction Ignore).Property  -contains $setting.name)
+    {
+        write-host "$($setting.name) is not correct"
+        $RemediationNeeded = $true
+    }
+}
+
+
+if ($RemediationNeeded -eq $true)
+{
+    write-host "registry settings are incorrect"
+    Stop-Transcript
+    exit 1
+}
+else 
+{
+    write-host "registry settings are correct"
+    Stop-Transcript
+    exit 0
+}
---
+++ AP_RemediationScript.ps1
@@ -0,0 +1,38 @@
+﻿$TranscriptPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+$TranscriptName = "AutoPatchRemediation.log"
+new-item $TranscriptPath -ItemType Directory -Force
+
+# stopping orphaned transcripts
+try
+{
+    stop-transcript|out-null
+}
+  catch [System.InvalidOperationException]
+{}
+
+Start-Transcript -Path $TranscriptPath\$TranscriptName -Append
+
+
+# initialize the array
+[PsObject[]]$regkeys = @()
+# populate the array with each object
+$regkeys += [PsObject]@{ Name = "DoNotConnectToWindowsUpdateInternetLocations"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"}
+$regkeys += [PsObject]@{ Name = "DisableWindowsUpdateAccess"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"}
+$regkeys += [PsObject]@{ Name = "WUServer"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"}
+$regkeys += [PsObject]@{ Name = "UseWUServer"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\"}
+$regkeys += [PsObject]@{ Name = "NoAutoUpdate"; path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\"}
+
+foreach ($setting in $regkeys)
+{
+    write-host "checking $($setting.name)"
+    if((Get-Item $setting.path -ErrorAction Ignore).Property -contains $setting.name)
+    {
+        write-host "remediating $($setting.name)"
+        Remove-ItemProperty -Path $setting.path -Name $($setting.name)
+    }
+    else
+    {
+        write-host "$($setting.name) was not found"
+    }
+}
+Stop-Transcript
